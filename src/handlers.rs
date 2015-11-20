325209904b079ae4c4e5ec0e815b31dacb7ec6ae
Michael Stock <mikeastock@gmail.com>
2015-11-19 20:55:06 -0800

    Pass response back

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,10 +1,14 @@
 use iron::prelude::*;
-use iron::status;
 use iron::{Handler};
+use iron::status;
+use iron::headers::{ContentType};
+use iron::mime::{Mime};
 
 use hyper::{Client};
+use hyper::client::IntoUrl;
 use hyper::header::{Headers, Authorization};
-use hyper::status::{StatusCode};
+use hyper::status as hyper_status;
+use std::io::Read;
 
 pub struct StatusCheckHandler;
 pub struct SwapTokenHandler;
@@ -14,7 +18,7 @@ static SPOTIFY_ACCOUNTS_ENDPOINT: &'static str = "https://accounts.spotify.com";
 
 impl Handler for StatusCheckHandler {
     fn handle(&self, _: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
+        Ok(Response::with((hyper_status::StatusCode::Ok, "Hello World!")))
     }
 }
 
@@ -25,27 +29,27 @@ impl Handler for SwapTokenHandler {
         let mut headers = Headers::new();
         headers.set(Authorization("Basic".to_owned()));
 
-        let res = client.post(SPOTIFY_ACCOUNTS_ENDPOINT)
+        let url = format!("{}/api/token", SPOTIFY_ACCOUNTS_ENDPOINT)
+            .into_url()
+            .unwrap();
+
+        let mut res = client.post(url)
             .headers(headers)
             .send()
             .unwrap();
 
-        match res.status {
-            StatusCode::Ok => {
-                println!("Success: {}", res.status);
-            }
+        let mut body = String::new();
 
-            _ => {
-                println!("Error");
-            }
-        }
+        res.read_to_string(&mut body).unwrap();
 
-        Ok(Response::with((res.status, "Hello World!")));
+        let mime: Mime = "application/json".parse().unwrap();
+
+        Ok(Response::with((res.status as status::Status, mime, body)))
     }
 }
 
 impl Handler for RefreshTokenHandler {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
+        Ok(Response::with((hyper_status::StatusCode::Ok, "Hello World!")))
     }
 }
